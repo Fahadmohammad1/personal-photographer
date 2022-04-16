0ea5fce2c527ff8dcddc822ba5b1a008dddd6f4f
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-04-17 03:30:18 +0600

    login routed set

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Services from "./components/Services/Services";
+import SignUp from "./components/SignUp/SignUp";
+import SignIn from "./components/SingIn/SignIn";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Home></Home>
-      <Services></Services>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/signIn" element={<SignIn></SignIn>}></Route>
+        <Route path="/signUp" element={<SignUp></SignUp>}></Route>
+      </Routes>
     </div>
   );
 }

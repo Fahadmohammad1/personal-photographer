8bf3ba3aac74d34af4a301bcfd3679527d0cf2eb
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-04-18 07:35:53 +0600

    blog and about added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./components/About/About";
+import Blogs from "./components/Blogs/Blogs";
 import CheckOut from "./components/CheckOut/CheckOut";
 
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
 import RequireAuth from "./components/RequireAuth/RequireAuth";
 
 import SignUp from "./components/SignUp/SignUp";
@@ -15,6 +18,8 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
         <Route path="/signIn" element={<SignIn></SignIn>}></Route>
         <Route path="/signUp" element={<SignUp></SignUp>}></Route>
         <Route
@@ -25,6 +30,7 @@ function App() {
             </RequireAuth>
           }
         ></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,27 @@
 import { signOut } from "firebase/auth";
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar, Spinner } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { NavLink } from "react-router-dom";
 import auth from "../../firebase.init";
 import "./Header.css";
 
 const Header = () => {
+  const [user, loading] = useAuthState(auth);
+  if (loading) {
+    return (
+      <Button variant="primary" disabled>
+        <Spinner
+          as="span"
+          animation="grow"
+          size="sm"
+          role="status"
+          aria-hidden="true"
+        />
+        Loading...
+      </Button>
+    );
+  }
   return (
     <div>
       <Navbar
@@ -21,25 +37,51 @@ const Header = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <NavLink to="/" className="text-dark">
+              <NavLink
+                to="/"
+                className="text-dark my-auto text-decoration-none"
+              >
                 Home
               </NavLink>
               <Nav.Link href="#services">Services</Nav.Link>
+              <NavLink
+                to="/blogs"
+                className="text-dark my-auto text-decoration-none"
+              >
+                Blogs
+              </NavLink>
+              <NavLink
+                to="/about"
+                className="text-dark my-auto text-decoration-none"
+              >
+                About
+              </NavLink>
             </Nav>
             <Nav>
-              <NavLink to="/signIn" className="text-dark">
-                SIGN IN
-              </NavLink>
-              <NavLink to="/signUp" className="text-dark">
-                SIGN UP
-              </NavLink>
-              <button
-                onClick={() => {
-                  signOut(auth);
-                }}
-              >
-                SIGN OUT
-              </button>
+              {user ? (
+                <button
+                  onClick={() => {
+                    signOut(auth);
+                  }}
+                >
+                  SIGN OUT
+                </button>
+              ) : (
+                <div>
+                  <NavLink
+                    to="/signIn"
+                    className="text-dark my-auto text-decoration-none"
+                  >
+                    SIGN IN
+                  </NavLink>
+                  <NavLink
+                    to="/signUp"
+                    className="text-dark my-auto text-decoration-none"
+                  >
+                    SIGN UP
+                  </NavLink>
+                </div>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
